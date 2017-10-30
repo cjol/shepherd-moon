fa4b0c1390a2c1430c7ea73888d0d90633aa7498
SaltySeaDog <jsavage93@gmail.com>
2017-10-30 18:30:18 +0000

    Removes PUblic Directory

--- public/imgs/rotate.bat
+++
@@ -1,5 +0,0 @@
-set img="IMG_8182.JPG"
-"D:\Program Files\ImageMagick-7.0.7-Q16\magick.exe" convert gallery/%img% -rotate 180 gallery/%img%
-
-set img="IMG_8165.JPG"
-"D:\Program Files\ImageMagick-7.0.7-Q16\magick.exe" convert gallery/%img% -rotate 180 gallery/%img%
